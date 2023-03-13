8a312c3089bae9d756dd2a804e7414cdc542bcd3
Vamshi77reddy <vamshireddy6563@gmail.com>
2023-03-13 12:23:22 +0530

    Added:UpdatesContacts

--- src/com/bridgeLabz/AddressBook.java
+++ src/com/bridgeLabz/AddressBook.java
@@ -15,6 +15,17 @@ public int createContact(Contacts contacts)
 	contactsOfPersons[index]=contacts;
 	return index;
 }
+public void updateContacts(String firstName) {
+
+	int i= findByFirstName(firstName);
+	if (i!=-1)
+	{
+		editContactDetails(contactsOfPersons[i]);
+	}
+	showAllContacts();
+
+}
+
 
 private int getIndexFornextEmptyLocation()
 {
@@ -27,7 +38,61 @@ private int getIndexFornextEmptyLocation()
 	}
 	return 0;
 }
+private void editContactDetails( Contacts contacts) {
+	Contacts con = new Contacts();
 
+	byte loopExit = 1; 
+		do {
+			System.out.println("Choose Option to Update :: " + '\n' + " 1.FirstName " + '\n' + " 2.LastName " + '\n' + " 3.Address "
+					+ '\n' + " 4.City " + '\n' + " 5.State " + '\n' + " 6.Zip " + '\n'
+							+ " 7.PhoneNumber " + '\n' + " 8.Aadhar_Number " + '\n' + " 9.Email_ID ");
+			byte option = getUserInput.nextByte();
+
+			switch(option) {
+			case 1:
+					System.out.println(" FirstName :: "); 
+					contacts.setFirstName(getUserInput.next());
+				break;
+			case 2:
+					System.out.println(" LastName :: ");
+					contacts.setLastName(getUserInput.next());
+				break;
+			case 3:
+					System.out.println(" PhoneNumber :: ");
+					contacts.setPhoneNumber(getUserInput.nextLong());
+
+				break;
+			case 4:
+				System.out.println(" Email_ID :: ");
+				contacts.setEmail(getUserInput.next());
+			break;
+			case 5:
+				System.out.println(" Address :: ");
+				con.setAddress(getUserInput.next());
+
+				break;
+			case 6:
+					System.out.println(" City :: ");
+					contacts.setCity(getUserInput.next());
+				break;
+			case 7:
+					System.out.println(" State :: ");
+					contacts.setState(getUserInput.next());
+				break;
+			case 8:
+					System.out.println(" Zip :: ");
+					contacts.setZip(getUserInput.nextInt());
+				break;
+
+
+
+			default:
+				System.out.println(" ****---- Invalid Input ----**** ");
+			}
+			System.out.println(" Press 1: If you want to update anything else " + '\n' + " Press 0: Exit to Menu ");
+			loopExit = getUserInput.nextByte();
+		}while(loopExit != 0);
+	}
 public void showAllContacts() {
 
 	System.out.println("Showing contacts details");
@@ -53,6 +118,20 @@ private void showContactDetails(Contacts contacts)
 
 	}
 }
+private int findByFirstName(String firstName)
+{
+
+	for (int i=0;i<contactsOfPersons.length;i++)
+	{								
+		Contacts contacts= contactsOfPersons[i];
+		if(null!=contacts && firstName.equalsIgnoreCase(contacts.getFirstName()))
+		{
+			return i;
+		}
+	}
+	return -1;
+
+}
 
 public static void main(String[] args) {
 	AddressBook adressBook=new AddressBook();
@@ -61,9 +140,11 @@ public static void main(String[] args) {
 	byte menueChoice=0;
 
 	do {
-		System.out.println("  ::>>>Adress Book<<<::  ");
+		System.out.println("  ::>>>Welcome Adress Book<<<::  ");
 		System.out.println("1.Add New Contcat");
 		System.out.println("2.View All Contacts");
+		System.out.println("3.Update Contact");
+
 		byte userChoice=getUserInput.nextByte();
 		switch (userChoice) 
 		{ 
@@ -75,6 +156,11 @@ public static void main(String[] args) {
 			break;
 		case 2:
 			adressBook.showAllContacts();
+			break;
+		case 3:
+			System.out.println("Enter First Name :: ");
+			String firstName = getUserInput.next();
+			adressBook.updateContacts(firstName);
 			break;
 		default:
 			System.out.println("Invalid Choice");
